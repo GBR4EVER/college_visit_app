2c9cb2975e12b430aae7b55538fb6440104cb27f
Corey <burkleycorey@yahoo.com>
2014-08-14 21:13:10 -0500

    mod

--- main.rb
+++ main.rb
@@ -10,14 +10,11 @@ require_relative "./models/student.rb"
 require_relative "./models/scholarship.rb"
 require_relative "./models/college.rb"
 
-$db = SQLite3::Database.new "college_visit_app.db"
-$db.results_as_hash = true
-
-require_relative "./student.rb"
+require "college_visit_app"
 
 
 get "/" do
-  @all_students = Student.all # This is a class-method that we made.
+  @students = Student.all # This is a class-method that we made.
   erb :home
 end
 
