fcdbca7e61eef16c616f643886aa752b37cdab2f
Corey <burkleycorey@yahoo.com>
2014-08-20 10:35:02 -0500

    added complete 7 file structure pattern excersize

--- main.rb
+++ main.rb
@@ -11,20 +11,46 @@ require_relative "./models/student.rb"
 require_relative "./models/scholarship.rb"
 require_relative "./models/college.rb"
 
-
-get "/" do
-  @students = Student.all # This is a class-method that we made.
-  erb :home
+# INDEX
+get "/students/index" do
+  @all_students = Student.all # This is a class-method that we made.
+  erb :student_index
 end
 
-get "/search" do
-  @colleges = College.all
-  erb :search
+#NEW
+get "/students/new" do
+  erb :student_new
 end
 
-get "/colleges/:id" do
-  @colleges = College.find(1)
-  erb :webpages
+#CREATE
+get "/students/create" do
+  @student = Student.create({name: params[:name], age: params[:age]})
+  erb :student_create
 end
 
-binding.pry
+#EDIT
+get "/students/:id/edit" do
+  @student = Student.find(params[:id])
+  erb :student_edit
+end
+
+#UPDATE
+post "/students/:id/update" do
+  @student = Student.find(params[:id])
+  @student.update.attributes({name: params[:name], age: params[:age]})
+  erb :student_update
+end
+
+#DELETE - Most of the time your delete will be a 'post' route.
+get "/students/:id/delete" do
+  @student = Student.find(params[:id]).delete
+  erb :delete
+end
+
+# SHOW
+get "/students/:id" do
+  @students = Student.find(params[:id])
+  erb :student_show
+end
+
+
