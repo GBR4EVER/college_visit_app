7b1fd9933092ff3c42992f8fcd341239adee1f57
Corey <burkleycorey@yahoo.com>
2014-08-20 10:51:47 -0500

    added colleges routes to main.rb

--- main.rb
+++ main.rb
@@ -44,7 +44,7 @@ end
 #DELETE - Most of the time your delete will be a 'post' route.
 get "/students/:id/delete" do
   @student = Student.find(params[:id]).delete
-  erb :delete
+  erb :student_delete
 end
 
 # SHOW
@@ -52,5 +52,51 @@ get "/students/:id" do
   @students = Student.find(params[:id])
   erb :student_show
 end
+
+#-------Colleges---------#
+
+# INDEX
+get "/colleges/index" do
+  @all_colleges = College.all # This is a class-method that we made.
+  erb :college_index
+end
+
+#NEW
+get "/colleges/new" do
+  erb :college_new
+end
+
+#CREATE
+get "/colleges/create" do
+  @college = College.create({college_id: params[:college_id], school: params[:school], nickname: params[:nickname] bio: params[:bio], tuition: params[:tuition], logo: params[:logo], address: params[:address], region: params[:region], webpage: params[:webpage]})
+  erb :college_create
+end
+
+#EDIT
+get "/colleges/:college_id/edit" do
+  @college = College.find(params[:college_id])
+  erb :college_edit
+end
+
+#UPDATE
+post "/colleges/:college_id/update" do
+  @college = College.find(params[:college_id])
+  @college.update.attributes({{college_id: params[:college_id], school: params[:school], nickname: params[:nickname] bio: params[:bio], tuition: params[:tuition], logo: params[:logo], address: params[:address], region: params[:region], webpage: params[:webpage]})
+  erb :college_update
+end
+
+#DELETE - Most of the time your delete will be a 'post' route.
+get "/colleges/:college_id/delete" do
+  @college = College.find(params[:college_id]).delete
+  erb :college_delete
+end
+
+# SHOW
+get "/colleges/:college_id" do
+  @colleges = College.find(params[:college_id])
+  erb :college_show
+end
+
+#-----------Scholarships-----------#
 
 
