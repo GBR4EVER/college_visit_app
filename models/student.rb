20b6403e8334de020ee6d585a433030d6ea27fc0
Corey <burkleycorey@yahoo.com>
2014-08-21 12:01:38 -0500

    mod

--- models/student.rb
+++ models/student.rb
@@ -3,4 +3,7 @@ class Student < ActiveRecord::Base
   has_and_belongs_to_many :colleges
   
   validates :name, :age, presence: true
+  validates :name, length: { minimum: 2}
+  validates :age, length: { maximum: 3}
+  validates :age, numericality: { only_integer: true }
 end
